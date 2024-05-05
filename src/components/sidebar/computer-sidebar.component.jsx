2fcc2e6f3aede0a2a90420a1ce404f7643e0b73a
void0xf <150953141+void0xf@users.noreply.github.com>
2024-05-05 14:47:22 +0200

    feat(User Email): added displaying real email instead of dummy one

--- src/components/sidebar/computer-sidebar.component.jsx
+++ src/components/sidebar/computer-sidebar.component.jsx
@@ -1,121 +1,186 @@
-import React, { useContext, useState, createContext} from 'react'
-import { TasksContext } from '../../contexts/tasks.context';
-import { TaskFilter } from '../../contexts/filter.context';
-import { CalendarDays, ChevronsRight, CircleDot, ListChecks, User, Briefcase, Menu, Search, StickyNote, LineChart, X, Edit, CalendarClock } from 'lucide-react';
-import { getLateTasks, getTasksForThisWeek, getTasksForToday } from '../../utils/task.utils';
-import AddNewList from './input/addNewList.component';
-import SearchTask from './searchTask.component';
-import Sidebar from './sidebar.component';
-import { SidebarItem } from './sidebar-item.component';
-import { SearchContext } from '../../contexts/search.context';
-import SidebarContext from '../../contexts/sidebar.context';
+import React, { useContext, useState, createContext } from "react";
+import { TasksContext } from "../../contexts/tasks.context";
+import { TaskFilter } from "../../contexts/filter.context";
+import {
+  CalendarDays,
+  ChevronsRight,
+  CircleDot,
+  ListChecks,
+  User,
+  Briefcase,
+  Menu,
+  Search,
+  StickyNote,
+  LineChart,
+  X,
+  Edit,
+  CalendarClock,
+} from "lucide-react";
+import {
+  getLateTasks,
+  getTasksForThisWeek,
+  getTasksForToday,
+} from "../../utils/task.utils";
+import AddNewList from "./input/addNewList.component";
+import SearchTask from "./searchTask.component";
+import Sidebar from "./sidebar.component";
+import { SidebarItem } from "./sidebar-item.component";
+import { SearchContext } from "../../contexts/search.context";
+import SidebarContext from "../../contexts/sidebar.context";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../../firebase/firebaseConfig";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const componentMap = {
-  'Personal': User,
-  'Work': Briefcase
+  Personal: User,
+  Work: Briefcase,
 };
 
 const ComputerSidebar = () => {
-  const SIZE_OF_SIDEBAR_ICONS  = 25;
-  const {state, dispatch} = useContext(TasksContext);
-  const {state: filterState} = useContext(TaskFilter);
-  const {state: searchState} = useContext(SearchContext);
-  const todayTasksCount = getTasksForToday(state.tasks, filterState.filterList).length;
+  const firebaseApp = initializeApp(firebaseConfig);
+  const firestore = getFirestore(firebaseApp);
+  const auth = getAuth(firebaseApp);
+  const [user] = useAuthState(auth);
+  const SIZE_OF_SIDEBAR_ICONS = 25;
+  const { state, dispatch } = useContext(TasksContext);
+  const { state: filterState } = useContext(TaskFilter);
+  const { state: searchState } = useContext(SearchContext);
+  const todayTasksCount = getTasksForToday(
+    state.tasks,
+    filterState.filterList
+  ).length;
   const { isSideBarActive, setIsSideBarActive } = useContext(SidebarContext);
-  const [isDeleteButtonActive, setIsDeleteButtonActive] = useState(false)
+  const [isDeleteButtonActive, setIsDeleteButtonActive] = useState(false);
 
   const handleListDelete = (name) => {
-    dispatch({type:'REMOVE_LIST', payload: name})
-  }
+    dispatch({ type: "REMOVE_LIST", payload: name });
+  };
 
   return (
-    
-    <div className={`relative z-20 h-screen transition-all duration-700 ease-in-out overflow-hidden 
-    ${isSideBarActive ? 'max-w-[280px]' : 'max-w-0'}`}>
-        <Sidebar>
-         <SearchTask />
-          <div><p className='text-sm font-semibold ml-3'>Tasks</p></div>
-            <SidebarItem 
-              icon={<ChevronsRight size={SIZE_OF_SIDEBAR_ICONS} strokeWidth={3}/>} 
-              text="Upcoming"
-              alert
-              numberOfAlerts={getTasksForThisWeek(state.tasks, filterState.listFilter, searchState.search).length}
-              clickType={'Upcoming'}
-              payload={'Upcoming'}
-              />
-            <SidebarItem 
-              icon={<ListChecks size={SIZE_OF_SIDEBAR_ICONS} alert/>} 
-              text="Today"
-              alert
-              clickType={'Today'}
-              payload={'Today'}
-              numberOfAlerts={todayTasksCount}
-              />
-              <SidebarItem 
-                icon={<CalendarClock size={SIZE_OF_SIDEBAR_ICONS} alert/>} 
-                text="Late"
-                alert
-                clickType={'Late'}
-                payload={'Late'}
-                numberOfAlerts={getLateTasks(state.tasks, filterState.listFilter, searchState.search).length}
+    <div
+      className={`relative z-20 h-screen transition-all duration-700 ease-in-out overflow-hidden 
+    ${isSideBarActive ? "max-w-[280px]" : "max-w-0"}`}
+    >
+      <Sidebar>
+        <SearchTask />
+        <div>
+          <p className="text-sm font-semibold ml-3">Tasks</p>
+        </div>
+        <SidebarItem
+          icon={<ChevronsRight size={SIZE_OF_SIDEBAR_ICONS} strokeWidth={3} />}
+          text="Upcoming"
+          alert
+          numberOfAlerts={
+            getTasksForThisWeek(
+              state.tasks,
+              filterState.listFilter,
+              searchState.search
+            ).length
+          }
+          clickType={"Upcoming"}
+          payload={"Upcoming"}
+        />
+        <SidebarItem
+          icon={<ListChecks size={SIZE_OF_SIDEBAR_ICONS} alert />}
+          text="Today"
+          alert
+          clickType={"Today"}
+          payload={"Today"}
+          numberOfAlerts={todayTasksCount}
+        />
+        <SidebarItem
+          icon={<CalendarClock size={SIZE_OF_SIDEBAR_ICONS} alert />}
+          text="Late"
+          alert
+          clickType={"Late"}
+          payload={"Late"}
+          numberOfAlerts={
+            getLateTasks(
+              state.tasks,
+              filterState.listFilter,
+              searchState.search
+            ).length
+          }
+        />
+        <SidebarItem
+          icon={<LineChart size={SIZE_OF_SIDEBAR_ICONS} alert />}
+          text="Statistics"
+          alert
+          clickType={"Statistics"}
+          payload={"Statistics"}
+        />
+        <SidebarItem
+          icon={<CalendarDays size={SIZE_OF_SIDEBAR_ICONS} />}
+          text="Calendar"
+          alert
+          clickType={"Calendar"}
+          payload={"Calendar"}
+        />
+        <SidebarItem
+          icon={<StickyNote size={SIZE_OF_SIDEBAR_ICONS} />}
+          text="Notes"
+          clickType={"Notes"}
+          payload={"Notes"}
+        />
+        <div className="border-t-2 border-b-2 border-bordercolor my-4 py-2 max-h-64 overflow-y-scroll overflow-x-hidden ">
+          <div className="flex justify-between items-center">
+            <p className="text-sm font-semibold ml-3">Lists</p>
+            <button
+              onClick={() => {
+                setIsDeleteButtonActive(!isDeleteButtonActive);
+              }}
+            >
+              <p className="mr-2">
+                <Edit size={20} />
+              </p>
+            </button>
+          </div>
+          {state.lists.map((listItem) => {
+            const IconComponent = componentMap[listItem.name];
+            return (
+              <div className="flex justify-between items-baseline">
+                <SidebarItem
+                  icon={
+                    IconComponent ? (
+                      <IconComponent size={SIZE_OF_SIDEBAR_ICONS} />
+                    ) : (
+                      <CircleDot size={SIZE_OF_SIDEBAR_ICONS} />
+                    )
+                  }
+                  text={listItem.name}
+                  clickType={"list"}
+                  payload={listItem.name}
+                  active={filterState.listFilter == listItem.name ? 1 : 0}
                 />
-              <SidebarItem 
-              icon={<LineChart  size={SIZE_OF_SIDEBAR_ICONS} alert/>} 
-              text="Statistics"
-              alert
-              clickType={'Statistics'}
-              payload={'Statistics'}
-              />
-            <SidebarItem 
-              icon={<CalendarDays size={SIZE_OF_SIDEBAR_ICONS}/>} 
-              text="Calendar" 
-              alert
-              clickType={'Calendar'}
-              payload={'Calendar'}
-              />
-            <SidebarItem 
-              icon={<StickyNote size={SIZE_OF_SIDEBAR_ICONS}/>} 
-              text="Notes"
-              clickType={'Notes'} 
-              payload={'Notes'}
-              />
-            <div className='border-t-2 border-b-2 border-bordercolor my-4 py-2 max-h-64 overflow-y-scroll overflow-x-hidden '>
-              <div className='flex justify-between items-center'>
-                <p className='text-sm font-semibold ml-3'>Lists</p> 
-                <button 
-                onClick={() => {setIsDeleteButtonActive(!isDeleteButtonActive)}}>
-                  <p className='mr-2'><Edit size={20}/></p>
-                </button>
+                <div
+                  className={`relative right-8 transition-opacity duration-600 ${
+                    isDeleteButtonActive ? "opacity-100" : "opacity-0"
+                  }`}
+                >
+                  {isDeleteButtonActive &&
+                  listItem.name != "Personal" &&
+                  listItem.name != "Work" ? (
+                    <button
+                      onClick={() => {
+                        handleListDelete(listItem.name);
+                      }}
+                    >
+                      <p>
+                        <X size={SIZE_OF_SIDEBAR_ICONS} />
+                      </p>
+                    </button>
+                  ) : null}
+                </div>
               </div>
-              {state.lists.map((listItem) => {
-                const IconComponent = componentMap[listItem.name];
-                return (
-                  <div className='flex justify-between items-baseline'>
-                    <SidebarItem 
-                      icon={IconComponent ? <IconComponent size={SIZE_OF_SIDEBAR_ICONS} /> : <CircleDot size={SIZE_OF_SIDEBAR_ICONS} />} 
-                      text={listItem.name}
-                      clickType={'list'}
-                      payload={listItem.name}
-                      active={filterState.listFilter == listItem.name ? 1 : 0}
-                    /> 
-                    <div className={`relative right-8 transition-opacity duration-600 ${isDeleteButtonActive ? 'opacity-100' : 'opacity-0'}`}>
-                      {
-                        isDeleteButtonActive && listItem.name != 'Personal' && listItem.name != 'Work'
-                        ?
-                          <button onClick={() => {handleListDelete(listItem.name)}}><p><X size={SIZE_OF_SIDEBAR_ICONS}/></p></button>
-                        :
-                        null
-                      }
-                    </div>
-                  </div>
-                );
-              }
-              )}
-              <AddNewList/>
-            </div>
-        </Sidebar> 
+            );
+          })}
+          <AddNewList />
+        </div>
+      </Sidebar>
     </div>
-  )
-}
+  );
+};
 
-export default ComputerSidebar
+export default ComputerSidebar;
--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -2,56 +2,73 @@ import { Menu, Settings } from "lucide-react";
 import SidebarContext from "../../contexts/sidebar.context";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
+import { getAuth } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "../../firebase/firebaseConfig";
 
 export default function Sidebar({ children }) {
-  const {isSideBarActive, setIsSideBarActive}  = useContext(SidebarContext);
-  const {dispatch} = useContext(UserContext)
+  const { isSideBarActive, setIsSideBarActive } = useContext(SidebarContext);
+  const firebaseApp = initializeApp(firebaseConfig);
+  const firestore = getFirestore(firebaseApp);
+  const auth = getAuth(firebaseApp);
+  const [user] = useAuthState(auth);
+  const { dispatch } = useContext(UserContext);
   const handleSettings = () => {
-    dispatch({type: 'TOGGLE_SETTINGS_CARD'})
-  }
+    dispatch({ type: "TOGGLE_SETTINGS_CARD" });
+  };
 
   return (
     <div className="h-full">
       <nav className="h-full flex flex-col bg-bkg border-bordercolor border-r shadow-sm justify-between">
         <div>
           <div className=" pb-2 flex justify-between items-center">
-            <div className="ml-2 font-semibold text-xl text-textcolor">Menu</div>
+            <div className="ml-2 font-semibold text-xl text-textcolor">
+              Menu
+            </div>
             <button
-              onClick={() => { setIsSideBarActive(false) }}
+              onClick={() => {
+                setIsSideBarActive(false);
+              }}
               className="p-1.5 rounded-lg bg-bkg hover:bg-text"
             >
-            <Menu />
+              <Menu />
             </button>
           </div>
           <div className="">{children}</div>
         </div>
-        
-        <button onClick={() => {handleSettings()}}>
-        <div className=" flex p-3">
-          <img
-            src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
-            alt=""
-            className="w-10 h-10 rounded-md"
-          />
-          <div
-            className={`
+
+        <button
+          onClick={() => {
+            handleSettings();
+          }}
+        >
+          <div className=" flex p-3">
+            <img
+              src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
+              alt=""
+              className="w-10 h-10 rounded-md"
+            />
+            <div
+              className={`
               flex justify-between items-center
               overflow-hidden transition-all w-52 ml-3
           `}
-          >
-            <div className="leading-4 flex flex-col items-start">
-              <h4 className="font-semibold">John Doe</h4>
-              <span className="text-xs text-gray-600">johndoe@gmail.com</span>
+            >
+              <div className="leading-4 flex flex-col items-start">
+                <h4 className="font-semibold">John Doe</h4>
+                <span className="text-xs text-gray-600">{user.email}</span>
+              </div>
+              <div>
+                <button>
+                  <Settings strokeWidth={1.7} size={20} />
+                </button>
+              </div>
             </div>
-          <div>
-            <button>
-              <Settings strokeWidth={1.7} size={20} />
-            </button>
           </div>
-          </div>
-        </div>
         </button>
       </nav>
     </div>
-  )
+  );
 }
