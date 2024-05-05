ef0384180455cab78d944f24d079135d80cdb189
void0xf <150953141+void0xf@users.noreply.github.com>
2024-05-05 14:49:02 +0200

    fix(Tasks): fixed displaying of old tasks

--- src/components/statistics/statisticsTabWeek.component.jsx
+++ src/components/statistics/statisticsTabWeek.component.jsx
@@ -1,35 +1,59 @@
-import React, { useContext } from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
-import { TasksContext } from '../../contexts/tasks.context';
-import { getTaskForDate, getTasksForThatDay, getTasksForThisWeek } from '../../utils/task.utils';
-import CustomTooltip from './customTooltip.component';
+import React, { useContext, useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { TasksContext } from "../../contexts/tasks.context";
+import {
+  getTaskForDate,
+  getTasksForThatDay,
+  getTasksForThisWeek,
+} from "../../utils/task.utils";
+import CustomTooltip from "./customTooltip.component";
 
 const StatisticsTabWeek = () => {
   const { state } = useContext(TasksContext);
-  const weekTasks = getTasksForThisWeek(state.completedTask);
+  const today = new Date();
+  const weekOldTasks = state.completedTask.filter(
+    (task) =>
+      task.taskDoneDate >=
+      new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7)
+  );
+
+  const [finalchar, setfinalchar] = useState();
   const chartData = {};
 
-  const options = {
-    weekday: 'long'
-  }
-  
-  weekTasks.forEach((task) => {
-    const day = task.taskDoneDate;
-    const tasksCompletedThatDay = getTaskForDate(state.completedTask, day);
-    chartData[day.toLocaleDateString('en-EN', options)] = tasksCompletedThatDay.length;
-  });
+  useEffect(() => {
+    const options = {
+      weekday: "long",
+    };
 
-  const data = Object.entries(chartData).map(([key, value]) => ({
-    Day: key,
-    TasksDone: value
-  }));
+    weekOldTasks.forEach((task) => {
+      const day = task.taskDoneDate;
+      const tasksCompletedThatDay = getTaskForDate(state.completedTask, day);
+      chartData[day.toLocaleDateString("en-EN", options)] =
+        tasksCompletedThatDay.length;
+    });
+
+    const data = Object.entries(chartData).map(([key, value]) => ({
+      Day: key,
+      TasksDone: value,
+    }));
+
+    setfinalchar(data);
+  }, [state.completedTask]);
 
   return (
-    <div className='flex flex-col'>
-      <div className='relative right-6'>
+    <div className="flex flex-col">
+      <div className="relative right-6">
         <ResponsiveContainer width="100%" height={400}>
-          <LineChart data={data}>
-          <Line type="monotone" dataKey="TasksDone" stroke="#aaaaaa" />
+          <LineChart data={finalchar}>
+            <Line type="monotone" dataKey="TasksDone" stroke="#aaaaaa" />
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
             <XAxis dataKey="Day" />
             <YAxis />
