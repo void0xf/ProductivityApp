5d750295fa716d92ce1ecad870c50336a2a39d92
void0xf <150953141+void0xf@users.noreply.github.com>
2024-05-05 15:54:31 +0200

    style(Upcoming): Fixed overflowing when added multiple tasks

--- src/components/upcomingTasks/upcoming.component.jsx
+++ src/components/upcomingTasks/upcoming.component.jsx
@@ -1,35 +1,69 @@
-import React, { useContext, useEffect, useState } from 'react'
-import ListOfTasksCard from './ListOfTasksCard.component'
-import { TasksContext } from '../../contexts/tasks.context'
-import { getTasksForThisWeek, getTasksForToday, getTasksForTommorow } from '../../utils/task.utils';
-import { TaskFilter } from '../../contexts/filter.context';
-import { SearchContext } from '../../contexts/search.context';
-import ComputerTitleCard from '../category-title-card/computer-title-card';
+import React, { useContext, useEffect, useState } from "react";
+import ListOfTasksCard from "./ListOfTasksCard.component";
+import { TasksContext } from "../../contexts/tasks.context";
+import {
+  getTasksForThisWeek,
+  getTasksForToday,
+  getTasksForTommorow,
+} from "../../utils/task.utils";
+import { TaskFilter } from "../../contexts/filter.context";
+import { SearchContext } from "../../contexts/search.context";
+import ComputerTitleCard from "../category-title-card/computer-title-card";
 
 const Upcoming = () => {
-  const {state} = useContext(TasksContext);
-  const {state: filterState} = useContext(TaskFilter);
-  const {state: searchState} = useContext(SearchContext);
-  const [todayTasks, setTodayTasks] = useState([])
-  const [tommorowTasks, setTommorowTasks] = useState([])
-  const [thisWeekTasks, setThisWeekTasks] = useState([])
+  const { state } = useContext(TasksContext);
+  const { state: filterState } = useContext(TaskFilter);
+  const { state: searchState } = useContext(SearchContext);
+  const [todayTasks, setTodayTasks] = useState([]);
+  const [tommorowTasks, setTommorowTasks] = useState([]);
+  const [thisWeekTasks, setThisWeekTasks] = useState([]);
 
   useEffect(() => {
-    setTodayTasks(getTasksForToday(state.tasks, filterState.listFilter, searchState.search));
-    setTommorowTasks(getTasksForTommorow(state.tasks, filterState.listFilter, searchState.search));
-    setThisWeekTasks(getTasksForThisWeek(state.tasks, filterState.listFilter, searchState.search));
-
-  }, [state.tasks, filterState, searchState])
+    setTodayTasks(
+      getTasksForToday(state.tasks, filterState.listFilter, searchState.search)
+    );
+    setTommorowTasks(
+      getTasksForTommorow(
+        state.tasks,
+        filterState.listFilter,
+        searchState.search
+      )
+    );
+    setThisWeekTasks(
+      getTasksForThisWeek(
+        state.tasks,
+        filterState.listFilter,
+        searchState.search
+      )
+    );
+  }, [state.tasks, filterState, searchState]);
 
   return (
-    <div className='sm: mx-4 h-screen'>
-      <ComputerTitleCard name={"Upcoming"} numberOfNotifictaions={thisWeekTasks.length > 0 ? thisWeekTasks.length : '-' }/>
-      
-      <ListOfTasksCard name={"Today"} tasks={todayTasks} addForTommorow={false}/>
-      <ListOfTasksCard name={"Tommorow"} tasks={tommorowTasks} addForTommorow={true}/>
-      <ListOfTasksCard name={"This Week"} tasks={thisWeekTasks} addForTommorow={false}/>
-    </div>
-  )
-}
+    <div className="sm: mx-4 h-screen sm:overflow-y-scroll sm:scroll-m-0 sm:no-scrollbar">
+      <ComputerTitleCard
+        name={"Upcoming"}
+        numberOfNotifictaions={
+          thisWeekTasks.length > 0 ? thisWeekTasks.length : "-"
+        }
+      />
 
-export default Upcoming
+      <ListOfTasksCard
+        name={"Today"}
+        tasks={todayTasks}
+        addForTommorow={false}
+      />
+      <ListOfTasksCard
+        name={"Tommorow"}
+        tasks={tommorowTasks}
+        addForTommorow={true}
+      />
+      <ListOfTasksCard
+        name={"This Week"}
+        tasks={thisWeekTasks}
+        addForTommorow={false}
+      />
+    </div>
+  );
+};
+
+export default Upcoming;
