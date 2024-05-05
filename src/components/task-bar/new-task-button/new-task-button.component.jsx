b6cb7d40e319dba827f6a78626323ba3aebcb3fc
void0xf <150953141+void0xf@users.noreply.github.com>
2024-05-05 15:53:36 +0200

    fix(TaskElement): removed text stacking, added max lenght for input

--- src/components/task-bar/new-task-button/new-task-button.component.jsx
+++ src/components/task-bar/new-task-button/new-task-button.component.jsx
@@ -1,82 +1,86 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useTaskContext } from '../../../contexts/tasks.context';
-import { v4 as uuidv4 } from 'uuid';
-import { TaskFilter } from '../../../contexts/filter.context';
-import axios from 'axios';
+import React, { useContext, useEffect, useState } from "react";
+import { useTaskContext } from "../../../contexts/tasks.context";
+import { v4 as uuidv4 } from "uuid";
+import { TaskFilter } from "../../../contexts/filter.context";
+import axios from "axios";
 import { RiBardFill } from "react-icons/ri";
-import { Plus } from 'lucide-react';
-import { askAi } from '../../../suggestionAI/data';
-import { getTasksForThisMonth } from '../../../utils/task.utils';
+import { Plus } from "lucide-react";
+import { askAi } from "../../../suggestionAI/data";
+import { getTasksForThisMonth } from "../../../utils/task.utils";
 
-
-const NewTaskButton = ({addForTommorow}) => {
+const NewTaskButton = ({ addForTommorow }) => {
   const uniqueId = uuidv4();
   const { state, dispatch } = useTaskContext();
-  const {state:filterState} = useContext(TaskFilter)
-  const [ counter, setCounter ] = useState(0);
-  const [ inputValue, setInputValue] = useState('')
-  const [ oldSuggestions, setOldSuggestions] = useState([]);
-  const [ isWaitingForResponse, setIsWaitingForResponse] = useState(false);
-  const [ currentFilterState, setCurrentFilterState] = useState('None')
+  const { state: filterState } = useContext(TaskFilter);
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const [oldSuggestions, setOldSuggestions] = useState([]);
+  const [isWaitingForResponse, setIsWaitingForResponse] = useState(false);
+  const [currentFilterState, setCurrentFilterState] = useState("None");
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
   const handleKeyPress = (event) => {
-    if(event.key === 'Enter' && inputValue.trim() !== '') {
+    if (event.key === "Enter" && inputValue.trim() !== "") {
       event.preventDefault();
       addNewTask();
-      setInputValue('');
+      setInputValue("");
     }
-  }
+  };
 
   const handleAISuggestion = async () => {
     setIsWaitingForResponse(true);
 
     const tasks = getTasksForThisMonth(state.tasks, filterState.listFilter);
     const taskNames = tasks.map((task) => task.taskName);
-    const taskNamesWithOldSuggestions = [...taskNames, ...oldSuggestions]
-    
-    const prompt = filterState.listFilter == 'None' 
-    ? `Suggest Me one Random task REPLY ONLY WITH TASK WITH NO SPECIAL CHARACTERS HERE IS THE TASKS I ALREADY HAVE 
+    const taskNamesWithOldSuggestions = [...taskNames, ...oldSuggestions];
+
+    const prompt =
+      filterState.listFilter == "None"
+        ? `Suggest Me one Random task REPLY ONLY WITH TASK WITH NO SPECIAL CHARACTERS HERE IS THE TASKS I ALREADY HAVE 
     ${oldSuggestions}`
-    : `Im Making ToDoList Called ${filterState.listFilter} suggest one random task based on that REPLY ONLY WITH TASK IN PLAIN TEXT WITH NO SPECIAL CHARACTERS AND NO BOILER PLATE LIKE 'HERE IS RANDOM TASK, Create a task to, Add a task, ETC' 
-    ${taskNamesWithOldSuggestions.length > 0 
-      ? `HERE IS THE TASKS I ALREADY HAVE ${taskNamesWithOldSuggestions}` 
-      : ''}`
-    
+        : `Im Making ToDoList Called ${
+            filterState.listFilter
+          } suggest one random task based on that REPLY ONLY WITH TASK IN PLAIN TEXT WITH NO SPECIAL CHARACTERS AND NO BOILER PLATE LIKE 'HERE IS RANDOM TASK, Create a task to, Add a task, ETC' 
+    ${
+      taskNamesWithOldSuggestions.length > 0
+        ? `HERE IS THE TASKS I ALREADY HAVE ${taskNamesWithOldSuggestions}`
+        : ""
+    }`;
+
     const newSuggestion = await askAi(prompt);
-    setOldSuggestions((prev) => [...prev, newSuggestion])
-    setIsWaitingForResponse(false);    
-  }
-  
+    setOldSuggestions((prev) => [...prev, newSuggestion]);
+    setIsWaitingForResponse(false);
+  };
+
   useEffect(() => {
     setOldSuggestions([]);
-  }, [filterState.listFilter])
+  }, [filterState.listFilter]);
 
   useEffect(() => {
-    setInputValue(oldSuggestions[oldSuggestions.length - 1])
-  }, [oldSuggestions])
+    setInputValue(oldSuggestions[oldSuggestions.length - 1]);
+  }, [oldSuggestions]);
 
   const addNewTask = () => {
-    const dateToSet =  new Date();
+    const dateToSet = new Date();
     if (addForTommorow) {
-      dateToSet.setDate(dateToSet.getDate() + 1); 
+      dateToSet.setDate(dateToSet.getDate() + 1);
     }
- 
+
     const newTask = {
       id: uniqueId,
       taskName: inputValue,
-      description: '',
+      description: "",
       date: dateToSet,
       list: filterState.listFilter,
       createDate: new Date(),
       doneDate: null,
       tags: [],
     };
-  
-    dispatch({ type: 'ADD_TASK', payload: newTask });
+
+    dispatch({ type: "ADD_TASK", payload: newTask });
   };
 
   const handleSubmit = (event) => {
@@ -84,33 +88,42 @@ const NewTaskButton = ({addForTommorow}) => {
   };
 
   return (
-    <div className=''>
+    <div className="">
       <div className="relative w-full">
         <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => handleInputChange(e)}
-          onKeyDown={(e) => handleKeyPress(e)}
-          placeholder='Add New Task'
-          className='pl-10 pr-4 py-2 border rounded-lg border-bordercolor focus:outline-acent w-full placeholder-grey-200 border-opacity-40 bg-bkg'
-        />
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => handleInputChange(e)}
+            onKeyDown={(e) => handleKeyPress(e)}
+            placeholder="Add New Task"
+            className="pl-10 pr-4 py-2 border rounded-lg border-bordercolor focus:outline-acent w-full placeholder-grey-200 border-opacity-40 bg-bkg"
+            maxLength={60}
+          />
         </form>
-       <div class="absolute inset-y-0 left-0 pl-3  
+        <div
+          class="absolute inset-y-0 left-0 pl-3  
                     flex items-center  
-                    pointer-events-none"> 
-             <Plus color='grey'/>
-      </div>
-        <div className='absolute right-3 top-3'>
-          <button onClick={() => {handleAISuggestion()}}>
-            <RiBardFill className={`transition-colors duration-700 ${isWaitingForResponse ? 'text-acent' : 'text-gray-600'}`} />
+                    pointer-events-none"
+        >
+          <Plus color="grey" />
+        </div>
+        <div className="absolute right-3 top-3">
+          <button
+            onClick={() => {
+              handleAISuggestion();
+            }}
+          >
+            <RiBardFill
+              className={`transition-colors duration-700 ${
+                isWaitingForResponse ? "text-acent" : "text-gray-600"
+              }`}
+            />
           </button>
-        </div> 
+        </div>
       </div>
-      
-
     </div>
   );
-}
+};
 
 export default NewTaskButton;
--- src/components/task-bar/taskElement.component.jsx
+++ src/components/task-bar/taskElement.component.jsx
@@ -1,15 +1,13 @@
-import React, { useState, useContext } from 'react';
-import { useSwipeable } from 'react-swipeable';
-import { Check, ChevronRight, Trash } from 'lucide-react';
-import { TasksContext } from '../../contexts/tasks.context';
-import { useSpring, animated } from 'react-spring';
-import { UserContext } from '../../contexts/user.context';
-
-
+import React, { useState, useContext } from "react";
+import { useSwipeable } from "react-swipeable";
+import { Check, ChevronRight, Trash } from "lucide-react";
+import { TasksContext } from "../../contexts/tasks.context";
+import { useSpring, animated } from "react-spring";
+import { UserContext } from "../../contexts/user.context";
 
 const TaskElement = ({ id, taskName }) => {
   const { dispatch } = useContext(TasksContext);
-  const {state} = useContext(UserContext)
+  const { state } = useContext(UserContext);
   const [isChecked, setIsChecked] = useState(false);
   const ANIMATION_DURATION = 300;
   const [offsetX, setOffsetX] = useState(0);
@@ -27,68 +25,64 @@ const TaskElement = ({ id, taskName }) => {
     onSwiped: (eventData) => handleSwiped(eventData),
     preventDefaultTouchmoveEvent: true,
     trackMouse: true,
-    config: {delta: 200},
+    config: { delta: 200 },
   });
 
   const handleDeleteTask = () => {
     setIsChecked(true);
     setTimeout(() => {
-      dispatch({ type: 'REMOVE_TASK_ID', payload: id });
+      dispatch({ type: "REMOVE_TASK_ID", payload: id });
     }, ANIMATION_DURATION);
   };
 
   const handleTaskDone = () => {
     setIsChecked(true);
     setTimeout(() => {
-      dispatch({ type: 'MARK_TASK_AS_DONE', payload: id });
+      dispatch({ type: "MARK_TASK_AS_DONE", payload: id });
     }, ANIMATION_DURATION);
-    if(state.vibrationOnTaskDone) {
-      if('vibrate' in navigator) {
-        navigator.vibrate(200); 
+    if (state.vibrationOnTaskDone) {
+      if ("vibrate" in navigator) {
+        navigator.vibrate(200);
       }
     }
-  }
+  };
 
   const handleSwiping = (eventData) => {
     setOffsetX(eventData.deltaX);
   };
 
   const handleSwiped = () => {
-    if(offsetX < -TRASH_THRESHOLD) {
+    if (offsetX < -TRASH_THRESHOLD) {
       handleDeleteTask();
     }
 
     if (offsetX > MAX_OFFSET_X) {
-      handleTaskDone() //ToDo make TaskDone;
+      handleTaskDone(); //ToDo make TaskDone;
     }
-    if(offsetX < -MAX_OFFSET_X) {
+    if (offsetX < -MAX_OFFSET_X) {
       setOffsetX(-MAX_OFFSET_X - 300);
     }
   };
 
   const handleOpenTaskTab = (taskId) => {
-    dispatch({ type: 'OPEN_TASK_TAB', payload: taskId });
+    dispatch({ type: "OPEN_TASK_TAB", payload: taskId });
   };
 
   return (
     <animated.div
       {...swipeHandlers}
-      className=
-      {`relative flex justify-between border-b-2 border-bordercolor py-2 rounded-md transition-opacity duration-${ANIMATION_DURATION} 
-      ${
-        isChecked ? 'opacity-0' : 'opacity-100'
-      }
-      ${offsetX < -TRASH_THRESHOLD ? 'bg-red-400' : 'bg-white-400'}
+      className={`relative flex justify-between border-b-2 border-bordercolor py-2 rounded-md transition-opacity duration-${ANIMATION_DURATION} 
+      ${isChecked ? "opacity-0" : "opacity-100"}
+      ${offsetX < -TRASH_THRESHOLD ? "bg-red-400" : "bg-white-400"}
       `}
-
       style={{
         transform: offsetXSpring.translateX.to((x) => `translateX(${x}px)`),
-        cursor: 'grab',
-        transition: 'opacity 1.3s ease',
+        cursor: "grab",
+        transition: "opacity 1.3s ease",
       }}
     >
-      <div className='flex items-center z-50'>
-        <div className='px-2'>
+      <div className="flex items-center z-50">
+        <div className="px-2">
           <label className="flex items-center cursor-pointer">
             <input
               type="checkbox"
@@ -102,28 +96,25 @@ const TaskElement = ({ id, taskName }) => {
             </div>
           </label>
         </div>
-        <div className='flex max-w-min'>
-          <div className={`${isChecked ? 'line-through' : ''}`}>
+        <div className="flex w-full">
+          <div className={`${isChecked ? "line-through" : ""}`}>
             <p>{taskName}</p>
           </div>
         </div>
       </div>
-      <div>
-      </div>
-      {offsetX < -TRASH_THRESHOLD 
-      ? 
-      (
-        <div className='bg-red-400 rounded-lg'>
+      <div></div>
+      {offsetX < -TRASH_THRESHOLD ? (
+        <div className="bg-red-400 rounded-lg">
           <Trash />
         </div>
-      )
-      :
-      (
-          <button className='p-2 transition' onClick={() => handleOpenTaskTab(id)}>
+      ) : (
+        <button
+          className="p-2 transition"
+          onClick={() => handleOpenTaskTab(id)}
+        >
           <ChevronRight />
-          </button>
-      )
-      }
+        </button>
+      )}
     </animated.div>
   );
 };
