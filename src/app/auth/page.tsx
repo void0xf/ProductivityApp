1e74aea43456c66751a1e406024c2485749aed5a
void0xf <lucyfer99100@gmail.com>
2025-04-07 18:42:44 +0200

    refactor: remove unused App and Auth components

--- src/app/auth/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import Authentication from "../../pages/AuthPage/Authentication";
-
-export default function AuthPage() {
-  return <Authentication />;
-}
